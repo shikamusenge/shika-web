4cc50f07542cd613ed0f6b77a5c4b0a9a39e2fa8
shikamusenge <shikamusenge720@gmail.com>
2024-08-15 18:51:36 +0200

    responsive-fixed

---
+++ .netlify/functions-internal/ssr/chunks/index_B3UmrKds.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_CU2I49Jc.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_CI4DjERu.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_Ba6eIXD7.mjs');
+
+export { page };
