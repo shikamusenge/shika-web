eed30c198ff68dc1b78813160451cb14721194ac
shikamusenge <shikamusenge720@gmail.com>
2024-08-15 14:01:22 +0200

    responsive-fixed

--- .netlify/functions-internal/ssr/chunks/index_CI4DjERu.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Ba6eIXD7.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_CThBcP92.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Bs8ls5SZ.mjs');
-
-export { page };
